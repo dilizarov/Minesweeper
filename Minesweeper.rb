e0028634153e11acad6fa719af3e90ec9d9906ff
David Ilizarov & Joe Averbukh <student@appacademy.io>
2013-07-22 12:00:59 -0700

    Headed to lunch. Just placed bombs into random tiles.

--- Minesweeper.rb
+++ Minesweeper.rb
@@ -10,18 +10,54 @@ class Board
       end
     end
 
-    grid
+    no_of_bombs = 0
+    until no_of_bombs == 10
+      tile = grid.flatten.sample
+      unless tile.bomb
+        tile.bomb = true
+        no_of_bombs += 1
+      end
+    end
   end
 
   def initialize
     @grid = Board.create_grid
   end
+
+  private
+
+  def place_bombs
+    positions = []
+    (0...9).each do |i|
+      (0...9).each do |j|
+        positions << [i,j]
+      end
+    end
+
+    no_of_bombs = 0
+    until no_of_bombs == 10
+      position = positions.sample
+
+  end
+
 end
+
 
 class Tile
 
+  attr_accessor :bomb
+
   def initialize(coord)
     @coord = coord
+  end
+
+  def bomb?
+  end
+
+  def reveal
+  end
+
+  def flag
   end
 
 end
